debdb5358f939f3839d58ca06671c447047b874d
uranuspucksaxophone <84784521+uranuspucksaxophone@users.noreply.github.com>
2021-10-31 14:30:23 +0100

    Updated copyright header from bisq to haveno

--- core/src/main/java/bisq/core/account/witness/AccountAgeWitnessService.java
+++ core/src/main/java/bisq/core/account/witness/AccountAgeWitnessService.java
@@ -1,5 +1,5 @@
 /*
- * This file is part of Bisq.
+ * This file is part of Haveno.
  *
  * bisq is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Affero General Public License as published by
