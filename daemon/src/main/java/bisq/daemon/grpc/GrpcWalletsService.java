234d4e2b22a13c4f9864e49cc47c2006939dcea1
woodser <woodser@protonmail.com>
2021-11-21 11:03:09 -0500

    relax grpc rate limits for tests

--- daemon/src/main/java/bisq/daemon/grpc/GrpcWalletsService.java
+++ daemon/src/main/java/bisq/daemon/grpc/GrpcWalletsService.java
@@ -318,7 +318,7 @@ class GrpcWalletsService extends WalletsImplBase {
         return getCustomRateMeteringInterceptor(coreApi.getConfig().appDataDir, this.getClass())
                 .or(() -> Optional.of(CallRateMeteringInterceptor.valueOf(
                         new HashMap<>() {{
-                            put(getGetBalancesMethod().getFullMethodName(), new GrpcCallRateMeter(10, SECONDS));
+                            put(getGetBalancesMethod().getFullMethodName(), new GrpcCallRateMeter(20, SECONDS));
                             put(getGetAddressBalanceMethod().getFullMethodName(), new GrpcCallRateMeter(1, SECONDS));
                             put(getGetFundingAddressesMethod().getFullMethodName(), new GrpcCallRateMeter(1, SECONDS));
                             put(getSendBtcMethod().getFullMethodName(), new GrpcCallRateMeter(1, MINUTES));
