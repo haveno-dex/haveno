6d8305c891c9eae6ea566c3fd331292d90fb7a68
jmacxx <47253594+jmacxx@users.noreply.github.com>
2022-08-18 10:50:27 -0500

    Prevent UI trading actions when there are no P2P connections.

--- desktop/src/main/java/bisq/desktop/util/GUIUtil.java
+++ desktop/src/main/java/bisq/desktop/util/GUIUtil.java
@@ -725,12 +725,11 @@ public class GUIUtil {
     }
 
     public static boolean isBootstrappedOrShowPopup(P2PService p2PService) {
-        if (!p2PService.isBootstrapped()) {
-            new Popup().information(Res.get("popup.warning.notFullyConnected")).show();
-            return false;
+        if (p2PService.isBootstrapped() && p2PService.getNumConnectedPeers().get() > 0) {
+            return true;
         }
-
-        return true;
+        new Popup().information(Res.get("popup.warning.notFullyConnected")).show();
+        return false;
     }
 
     public static boolean isReadyForTxBroadcastOrShowPopup(P2PService p2PService, CoreMoneroConnectionsService connectionService) {
