a4109cd4c7ed063fa9d0bc766f2f12d3f0098cb1
woodser <woodser@protonmail.com>
2023-04-26 16:48:34 -0400

    fix log on connection changed

--- core/src/main/java/haveno/core/api/CoreMoneroConnectionsService.java
+++ core/src/main/java/haveno/core/api/CoreMoneroConnectionsService.java
@@ -481,7 +481,7 @@ public final class CoreMoneroConnectionsService {
     }
 
     private void onConnectionChanged(MoneroRpcConnection currentConnection) {
-        log.info("CoreMoneroConnetionsService.onConnectionChanged() uri={}, connected=", currentConnection == null ? null : currentConnection.getUri(), currentConnection == null ? "false" : currentConnection.isConnected());
+        log.info("CoreMoneroConnectionsService.onConnectionChanged() uri={}, connected={}", currentConnection == null ? null : currentConnection.getUri(), currentConnection == null ? "false" : currentConnection.isConnected());
         if (isShutDownStarted) return;
         synchronized (lock) {
             if (currentConnection == null) {
