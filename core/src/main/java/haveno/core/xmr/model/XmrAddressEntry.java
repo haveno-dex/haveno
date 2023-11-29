aba6ce71232c626589a0f0abf79ce91e9ce7781d
woodser <woodser@protonmail.com>
2023-11-29 10:28:28 -0500

    hide reserved subaddresses in deposit view

--- core/src/main/java/haveno/core/xmr/model/XmrAddressEntry.java
+++ core/src/main/java/haveno/core/xmr/model/XmrAddressEntry.java
@@ -37,12 +37,17 @@ import java.util.Optional;
 @EqualsAndHashCode
 @Slf4j
 public final class XmrAddressEntry implements PersistablePayload {
+
     public enum Context {
         ARBITRATOR,
         BASE_ADDRESS,
         AVAILABLE,
         OFFER_FUNDING,
-        TRADE_PAYOUT
+        TRADE_PAYOUT;
+
+        public boolean isReserved() {
+            return this == Context.OFFER_FUNDING || this == Context.TRADE_PAYOUT;
+        }
     }
 
     // keyPair can be null in case the object is created from deserialization as it is transient.
