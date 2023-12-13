d6f84241d02a32f5494c7714a095f0bf55ca345c
woodser <woodser@protonmail.com>
2023-12-13 10:12:02 -0500

    log price provider

--- core/src/main/java/haveno/core/provider/ProvidersRepository.java
+++ core/src/main/java/haveno/core/provider/ProvidersRepository.java
@@ -95,6 +95,7 @@ public class ProvidersRepository {
 
             // update base url
             baseUrl = providerList.get(index);
+            log.info("Selected price provider: " + baseUrl);
 
             if (providerList.size() == 1 && config.baseCurrencyNetwork.isMainnet())
                 log.warn("We only have one provider");
