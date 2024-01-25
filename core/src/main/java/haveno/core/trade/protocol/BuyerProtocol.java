3b536047e95b089236dfe0e09130c6283c05e33f
woodser <woodser@protonmail.com>
2024-01-25 11:43:53 -0500

    check if trade shut down started within lock

--- core/src/main/java/haveno/core/trade/protocol/BuyerProtocol.java
+++ core/src/main/java/haveno/core/trade/protocol/BuyerProtocol.java
@@ -54,11 +54,9 @@ public class BuyerProtocol extends DisputeProtocol {
     protected void onInitialized() {
         super.onInitialized();
 
-        // done if shut down
-        if (trade.isShutDown()) return;
-
         // re-send payment sent message if not acked
         synchronized (trade) {
+            if (trade.isShutDownStarted()) return;
             if (trade.getState().ordinal() >= Trade.State.BUYER_SENT_PAYMENT_SENT_MSG.ordinal() && trade.getState().ordinal() < Trade.State.SELLER_RECEIVED_PAYMENT_SENT_MSG.ordinal()) {
                 latchTrade();
                 given(anyPhase(Trade.Phase.PAYMENT_SENT)
