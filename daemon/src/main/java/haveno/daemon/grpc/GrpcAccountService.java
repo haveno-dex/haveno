63677c638c4c5320864f5b29b0099d7c6faffb8f
woodser <woodser@protonmail.com>
2024-02-13 13:38:18 -0500

    revert bisq licenses and add haveno licenses

--- daemon/src/main/java/haveno/daemon/grpc/GrpcAccountService.java
+++ daemon/src/main/java/haveno/daemon/grpc/GrpcAccountService.java
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with Haveno. If not, see <http://www.gnu.org/licenses/>.
  */
+
 package haveno.daemon.grpc;
 
 import com.google.common.annotations.VisibleForTesting;
