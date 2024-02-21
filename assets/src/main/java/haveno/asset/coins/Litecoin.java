8a20634e37f345dd4b259e462f8f39a9e16feda7
OrginalS <36825453+OrginalS@users.noreply.github.com>
2024-02-21 01:44:31 +0100

    Update Litecoin.java

--- assets/src/main/java/haveno/asset/coins/Litecoin.java
+++ assets/src/main/java/haveno/asset/coins/Litecoin.java
@@ -1,35 +1,28 @@
 /*
- * This file is part of Bisq.
+ * This file is part of Haveno.
  *
- * Bisq is free software: you can redistribute it and/or modify it
+ * Haveno is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or (at
  * your option) any later version.
  *
- * Bisq is distributed in the hope that it will be useful, but WITHOUT
+ * Haveno is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
+ * along with Haveno. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package haveno.asset.coins;
 
-import haveno.asset.Base58AddressValidator;
+import haveno.asset.LitecoinAddressValidator;
 import haveno.asset.Coin;
-import haveno.asset.NetworkParametersAdapter;
 
 public class Litecoin extends Coin {
-    public Litecoin() {
-        super("Litecoin", "LTC", new Base58AddressValidator(new LitecoinMainNetParams()), Network.MAINNET);
-    }
 
-    public static class LitecoinMainNetParams extends NetworkParametersAdapter {
-        public LitecoinMainNetParams() {
-            this.addressHeader = 48;
-            this.p2shHeader = 5;
-        }
+    public Litecoin() {
+        super("Litecoin", "LTC", new LitecoinAddressValidator());
     }
 }
