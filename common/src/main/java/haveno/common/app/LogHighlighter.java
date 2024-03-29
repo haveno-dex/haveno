53001823af3e04461dcd0bf472c0a73054fc5909
woodser <woodser@protonmail.com>
2024-03-29 07:07:12 -0400

    update logback and add license

--- common/src/main/java/haveno/common/app/LogHighlighter.java
+++ common/src/main/java/haveno/common/app/LogHighlighter.java
@@ -1,3 +1,17 @@
+/**
+ * Logback: the reliable, generic, fast and flexible logging framework.
+ * Copyright (C) 1999-2015, QOS.ch. All rights reserved.
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
+ *
+ *   or (per the licensee's choosing)
+ *
+ * under the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation.
+ */
+
 /* Derived from https://logback.qos.ch/xref/ch/qos/logback/classic/pattern/color/HighlightingCompositeConverter.html */
 
 package haveno.common.app;
