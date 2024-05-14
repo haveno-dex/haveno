4ecfe15714b3093b8711fa3e659f94a5b19259ec
retoaccess1 <retoaccess@proton.me>
2024-05-14 16:49:36 +0200

    Update ArbitratorManager.java

--- core/src/main/java/haveno/core/support/dispute/arbitration/arbitrator/ArbitratorManager.java
+++ core/src/main/java/haveno/core/support/dispute/arbitration/arbitrator/ArbitratorManager.java
@@ -42,7 +42,7 @@ import haveno.core.filter.FilterManager;
 import haveno.core.support.dispute.agent.DisputeAgentManager;
 import haveno.core.user.User;
 import haveno.network.p2p.storage.payload.ProtectedStorageEntry;
-import java.util.ArrayList;
+//import java.util.ArrayList;TODO
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 
@@ -79,7 +79,10 @@ public class ArbitratorManager extends DisputeAgentManager<Arbitrator> {
                     "02a1a458df5acf4ab08fdca748e28f33a955a30854c8c1a831ee733dca7f0d2fcd",
                     "0374dd70f3fa6e47ec5ab97932e1cec6233e98e6ae3129036b17118650c44fd3de");
         case XMR_MAINNET:
-            return new ArrayList<String>();
+            //return new ArrayList<String>();
+            return List.of(
+                    "029da09bc04dea33cd11a31bc1c05aa830b9180acb84e5370ee7fde60cae9f3d03",
+                    "02834de139c2767cd11f000f8ea71a3e168fec81132850a4a2cce65385da57a98a");
         default:
             throw new RuntimeException("Unhandled base currency network: " + Config.baseCurrencyNetwork());
         }
