610a7c586fae3749a2a066075e7d4ba58992bce5
woodser <woodser@protonmail.com>
2024-05-15 17:16:52 -0400

    fix concurrent modification exception in spread view

--- desktop/src/main/java/haveno/desktop/main/market/spread/SpreadViewModel.java
+++ desktop/src/main/java/haveno/desktop/main/market/spread/SpreadViewModel.java
@@ -113,18 +113,20 @@ class SpreadViewModel extends ActivatableViewModel {
 
     private void update(ObservableList<OfferBookListItem> offerBookListItems) {
         Map<String, List<Offer>> offersByCurrencyMap = new HashMap<>();
-        for (OfferBookListItem offerBookListItem : offerBookListItems) {
-            Offer offer = offerBookListItem.getOffer();
-            String key = offer.getCurrencyCode();
-            if (includePaymentMethod) {
-                key = offer.getPaymentMethod().getShortName();
-                if (expandedView) {
-                    key += ":" + offer.getCurrencyCode();
+        synchronized (offerBookListItems) {
+            for (OfferBookListItem offerBookListItem : offerBookListItems) {
+                Offer offer = offerBookListItem.getOffer();
+                String key = offer.getCurrencyCode();
+                if (includePaymentMethod) {
+                    key = offer.getPaymentMethod().getShortName();
+                    if (expandedView) {
+                        key += ":" + offer.getCurrencyCode();
+                    }
                 }
+                if (!offersByCurrencyMap.containsKey(key))
+                    offersByCurrencyMap.put(key, new ArrayList<>());
+                offersByCurrencyMap.get(key).add(offer);
             }
-            if (!offersByCurrencyMap.containsKey(key))
-                offersByCurrencyMap.put(key, new ArrayList<>());
-            offersByCurrencyMap.get(key).add(offer);
         }
         spreadItems.clear();
 
