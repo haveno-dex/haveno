4172cc72dff9049a8ad6c159ccfff36c0bf96b6a
woodser <woodser@protonmail.com>
2024-05-23 17:09:22 -0400

    avoid unused import by using List.of()

--- core/src/main/java/haveno/core/support/dispute/arbitration/arbitrator/ArbitratorManager.java
+++ core/src/main/java/haveno/core/support/dispute/arbitration/arbitrator/ArbitratorManager.java
@@ -42,7 +42,6 @@ import haveno.core.filter.FilterManager;
 import haveno.core.support.dispute.agent.DisputeAgentManager;
 import haveno.core.user.User;
 import haveno.network.p2p.storage.payload.ProtectedStorageEntry;
-import java.util.ArrayList;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 
@@ -79,7 +78,7 @@ public class ArbitratorManager extends DisputeAgentManager<Arbitrator> {
                     "02a1a458df5acf4ab08fdca748e28f33a955a30854c8c1a831ee733dca7f0d2fcd",
                     "0374dd70f3fa6e47ec5ab97932e1cec6233e98e6ae3129036b17118650c44fd3de");
         case XMR_MAINNET:
-            return new ArrayList<String>();
+            return List.of();
         default:
             throw new RuntimeException("Unhandled base currency network: " + Config.baseCurrencyNetwork());
         }
