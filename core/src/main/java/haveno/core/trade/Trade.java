69a1e67da2b2d0ccb658fb8c97d4768622ac92e9
woodser <woodser@protonmail.com>
2024-06-20 11:19:28 -0400

    remove incomplete trades from trade summary to fix #1029

--- core/src/main/java/haveno/core/trade/Trade.java
+++ core/src/main/java/haveno/core/trade/Trade.java
@@ -2180,7 +2180,7 @@ public abstract class Trade implements Tradable, Model {
         return tradeAmountTransferred();
     }
 
-    private boolean tradeAmountTransferred() {
+    public boolean tradeAmountTransferred() {
         return isPaymentReceived() || (getDisputeResult() != null && getDisputeResult().getWinner() == DisputeResult.Winner.SELLER);
     }
 
