856faafd1ce45aead9648d000d133048298f216e
fa2a5qj3 <174058787+fa2a5qj3@users.noreply.github.com>
2024-06-30 08:56:39 -0400

    fix IPv6 connectivity to XMR nodes #1076

--- core/src/main/java/haveno/core/xmr/nodes/XmrNodes.java
+++ core/src/main/java/haveno/core/xmr/nodes/XmrNodes.java
@@ -150,7 +150,7 @@ public class XmrNodes {
             if (parts[0].contains("[") && parts[0].contains(":")) {
                 // IPv6 address and optional port number
                 // address part delimited by square brackets e.g. [2a01:123:456:789::2]:8333
-                host = parts[0].replace("[", "").replace("]", "");
+                host = parts[0] + "]";  // keep the square brackets per RFC-2732
                 if (parts.length == 2)
                     port = Integer.parseInt(parts[1].replace(":", ""));
             } else if (parts[0].contains(":") && !parts[0].contains(".")) {
