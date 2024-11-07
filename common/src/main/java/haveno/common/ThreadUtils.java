abe370c89d35a360dfb4b100da7f503e1cec9310
XMRZombie <monerozombie@proton.me>
2024-11-07 17:35:13 +0000

    Update ThreadUtils.java
    Fixed imports

--- common/src/main/java/haveno/common/ThreadUtils.java
+++ common/src/main/java/haveno/common/ThreadUtils.java
@@ -1,3 +1,36 @@
+/*
+* This file is part of Bisq.
+*
+* Bisq is free software: you can redistribute it and/or modify it
+* under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or (at
+* your option) any later version.
+*
+* Bisq is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+* License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Bisq. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package haveno.common;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Utility class for managing threads and executors in a thread-safe way.
+ */
 public class ThreadUtils {
 
     private static final Map<String, ExecutorService> EXECUTORS = new HashMap<>();
@@ -105,9 +138,9 @@ public class ThreadUtils {
         }
     }
 
-    // TODO: consolidate and cleanup apis
-    
-    // Task submission to the default pool
+        // TODO: consolidate and cleanup apis
+
+   // Task submission to the default pool
     public static Future<?> submitToPool(Runnable task) {
         return submitToPool(Arrays.asList(task)).get(0);
     }
