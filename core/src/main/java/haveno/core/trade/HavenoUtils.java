0aad2457439f5c80a8be94018865018c25159a54
retoaccess1 <retoaccess@proton.me>
2024-11-12 17:35:12 +0000

    Remove fees
    Set fees to 0%

--- core/src/main/java/haveno/core/trade/HavenoUtils.java
+++ core/src/main/java/haveno/core/trade/HavenoUtils.java
@@ -91,9 +91,9 @@ public class HavenoUtils {
 
     // configure fees
     public static final boolean ARBITRATOR_ASSIGNS_TRADE_FEE_ADDRESS = true;
-    public static final double PENALTY_FEE_PCT = 0.02; // 2%
     public static final double MAKER_FEE_PCT = 0.00; // 0%
     public static final double TAKER_FEE_PCT = 0.00; // 0%
+    public static final double PENALTY_FEE_PCT = 0.02; // 2%
     public static final double MAKER_FEE_FOR_TAKER_WITHOUT_DEPOSIT_PCT = MAKER_FEE_PCT + TAKER_FEE_PCT; // customize maker's fee when no deposit or fee from taker
 
     // other configuration
