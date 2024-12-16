192eeb9b0d4ce83e9bfb9df37e6384456414079f
XMRZombie <monerozombie@proton.me>
2024-12-16 13:07:53 +0000

    Update haveno_paths.sh

--- scripts/deployment/haveno_paths.sh
+++ scripts/deployment/haveno_paths.sh
@@ -3,5 +3,5 @@ HAVENO_BIN=$(find /opt/haveno/bin/ -type f -executable -name "Haveno" 2>/dev/nul
 HAVENO_WALLET=$(find ~/.local/share/Haveno -type d -name "wallet" 2>/dev/null)
 echo "creating symlink to" $HAVENO_BIN "in" /bin
 echo "creating symlink to" $HAVENO_WALLET "in" ~/Monero/wallets/Haveno_wallets/
-ln -s $HAVENO_BIN ~/bin/Haveno
+ln -s $HAVENO_BIN /bin/Haveno
 ln -s $HAVENO_WALLET ~/Monero/wallets/Haveno_wallets/
