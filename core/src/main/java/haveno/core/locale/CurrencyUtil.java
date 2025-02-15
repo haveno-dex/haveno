fa9c9b78decac8c075d0629aa5ef124b9a37cb08
woodser <woodser@protonmail.com>
2025-02-15 07:59:38 -0500

    add usdt-trc20 as main crypto currency

--- core/src/main/java/haveno/core/locale/CurrencyUtil.java
+++ core/src/main/java/haveno/core/locale/CurrencyUtil.java
@@ -201,6 +201,7 @@ public class CurrencyUtil {
         result.add(new CryptoCurrency("ETH", "Ether"));
         result.add(new CryptoCurrency("LTC", "Litecoin"));
         result.add(new CryptoCurrency("USDT-ERC20", "Tether USD (ERC20)"));
+        result.add(new CryptoCurrency("USDT-TRC20", "Tether USD (TRC20)"));
         result.add(new CryptoCurrency("USDC-ERC20", "USD Coin (ERC20)"));
         result.sort(TradeCurrency::compareTo);
         return result;
