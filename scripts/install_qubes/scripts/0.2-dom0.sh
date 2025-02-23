8402ecbd59b50b123869b09c4646fb004b2bb86f
PromptPunksFauxCough <PromptPunksFauxCough@proton.me>
2025-02-23 12:05:52 +0000

    oops

--- scripts/install_qubes/scripts/0.2-dom0.sh
+++ scripts/install_qubes/scripts/0.2-dom0.sh
@@ -2,6 +2,6 @@
 ## ./haveno-on-qubes/scripts/0.2-dom0.sh
 
 ## Create Haveno AppVM:
-qvm-create --template haveno-template --class AppVM --label=orange --property memory=2048 --property maxmem=4096 --property netvm=sys-haveno haveno && qvm-start haveno-template
+qvm-create --template haveno-template --class AppVM --label=orange --property memory=2048 --property maxmem=4096 --property netvm=sys-haveno haveno && qvm-start haveno
 printf 'haveno-Haveno.desktop' | qvm-appmenus --set-whitelist - haveno
 
