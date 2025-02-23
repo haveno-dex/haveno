e00926c79c3db50a5d8f4d30af3c736feea5858a
PromptPunksFauxCough <PromptPunksFauxCough@proton.me>
2025-02-23 10:53:44 +0000

    forgot to set preference

--- scripts/install_qubes/scripts/0.1-dom0.sh
+++ scripts/install_qubes/scripts/0.1-dom0.sh
@@ -2,5 +2,5 @@
 ## ./haveno-on-qubes/scripts/0.1-dom0.sh
 
 ## Create Haveno NetVM:
-qvm-create --template whonix-gateway-17 --class AppVM --label=orange --property memory=512 --property maxmem=512 --property netvm=sys-firewall sys-haveno && qvm-start sys-haveno
+qvm-create --template whonix-gateway-17 --class AppVM --label=orange --property memory=512 --property maxmem=512 --property netvm=sys-firewall sys-haveno && qvm-prefs --set sys-haveno provides_network True && qvm-start sys-haveno
 
