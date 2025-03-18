a7d8e4560f84d4f31460dacbb8ab6ab7fdace2e2
woodser <13068859+woodser@users.noreply.github.com>
2025-03-18 19:10:56 -0400

    enable resending payment received msgs based on offer protocol version

--- core/src/main/java/haveno/core/trade/protocol/SellerProtocol.java
+++ core/src/main/java/haveno/core/trade/protocol/SellerProtocol.java
@@ -54,8 +54,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class SellerProtocol extends DisputeProtocol {
     
-    private static final long RESEND_PAYMENT_RECEIVED_MSGS_AFTER = 1741629525730L; // Mar 10 2025 17:58 UTC
-
     enum SellerEvent implements FluentProtocol.Event {
         STARTUP,
         DEPOSIT_TXS_CONFIRMED,
@@ -100,7 +98,7 @@ public class SellerProtocol extends DisputeProtocol {
     }
 
     private boolean resendPaymentReceivedMessagesEnabled() {
-        return trade.getTakeOfferDate().getTime() > RESEND_PAYMENT_RECEIVED_MSGS_AFTER;
+        return trade.getOffer().getOfferPayload().getProtocolVersion() >= 2;
     }
 
     @Override
