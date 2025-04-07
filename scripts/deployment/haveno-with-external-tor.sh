6de43bd5bc5e34ce4a529e9e7f750d6354d727f1
XMRZombie <monerozombie@proton.me>
2025-04-07 17:57:50 +0000

    Update haveno-with-external-tor.sh
    detaching tor flag

--- scripts/deployment/haveno-with-external-tor.sh
+++ scripts/deployment/haveno-with-external-tor.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 HAVENOHOME=$(find $HOME/.local/share -type d -name Haveno*)
 TORHOME="$HAVENOHOME/xmr_mainnet/tor"
-tor -f $TORHOME/torrc --ControlPortWriteToFile $TORHOME/.tor/control.port --DisableNetwork 0
+tor -f $TORHOME/torrc --ControlPortWriteToFile $TORHOME/.tor/control.port --DisableNetwork 0 --RunAsDaemon 1
 /opt/haveno/bin/Haveno --torControlUseSafeCookieAuth --torControlCookieFile=$TORHOME/.tor/control_auth_cookie --torControlPort=$(cat $TORHOME/.tor/control.port | sed 's/.*:\([0-9]\+\)/\1/')
 kill $(cat $TORHOME/pid)
