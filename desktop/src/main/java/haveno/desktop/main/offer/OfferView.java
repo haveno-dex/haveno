63dca46e271444838d0a927b30dba051c0cfd667
woodser <13068859+woodser@users.noreply.github.com>
2025-05-03 14:53:51 -0400

    'buy monero with' is bigger

--- desktop/src/main/java/haveno/desktop/main/offer/OfferView.java
+++ desktop/src/main/java/haveno/desktop/main/offer/OfferView.java
@@ -220,7 +220,7 @@ public abstract class OfferView extends ActivatableView<TabPane, Void> {
                     labelTab.setClosable(false);
                     Label offerLabel = new Label(getOfferLabel()); // use overlay for label for custom formatting
                     offerLabel.getStyleClass().add("titled-group-bg-label");
-                    offerLabel.setStyle("-fx-font-size: 1.1em;");
+                    offerLabel.setStyle("-fx-font-size: 1.3em;");
                     labelTab.setGraphic(offerLabel);
 
                     fiatOfferBookTab = new Tab(Res.get("shared.fiat"));
