21aafb15b697b6290f6b089253a70d6b6396dace
woodser <13068859+woodser@users.noreply.github.com>
2025-05-17 15:27:44 -0400

    spread view columns highlighted

--- desktop/src/main/java/haveno/desktop/components/ColoredDecimalPlacesWithZerosText.java
+++ desktop/src/main/java/haveno/desktop/components/ColoredDecimalPlacesWithZerosText.java
@@ -26,33 +26,37 @@ import javafx.scene.layout.HBox;
 public class ColoredDecimalPlacesWithZerosText extends HBox {
 
     public ColoredDecimalPlacesWithZerosText(String number, int numberOfZerosToColorize) {
+        this(number, numberOfZerosToColorize, false);
+    }
+
+    public ColoredDecimalPlacesWithZerosText(String number, int numberOfZerosToColorize, boolean highlight) {
         super();
 
         if (numberOfZerosToColorize <= 0) {
             getChildren().addAll(new Label(number));
         } else if (number.contains(FormattingUtils.RANGE_SEPARATOR)) {
             String[] splitNumber = number.split(FormattingUtils.RANGE_SEPARATOR);
-            Tuple2<Label, Label> numbers = getSplittedNumberNodes(splitNumber[0], numberOfZerosToColorize);
+            Tuple2<Label, Label> numbers = getSplittedNumberNodes(splitNumber[0], numberOfZerosToColorize, highlight);
             getChildren().addAll(numbers.first, numbers.second);
 
             getChildren().add(new Label(FormattingUtils.RANGE_SEPARATOR));
 
-            numbers = getSplittedNumberNodes(splitNumber[1], numberOfZerosToColorize);
+            numbers = getSplittedNumberNodes(splitNumber[1], numberOfZerosToColorize, highlight);
             getChildren().addAll(numbers.first, numbers.second);
         } else {
-            Tuple2<Label, Label> numbers = getSplittedNumberNodes(number, numberOfZerosToColorize);
+            Tuple2<Label, Label> numbers = getSplittedNumberNodes(number, numberOfZerosToColorize, highlight);
             getChildren().addAll(numbers.first, numbers.second);
         }
         setAlignment(Pos.CENTER_LEFT);
     }
 
-    private Tuple2<Label, Label> getSplittedNumberNodes(String number, int numberOfZeros) {
+    private Tuple2<Label, Label> getSplittedNumberNodes(String number, int numberOfZeros, boolean highlight) {
         String placesBeforeZero = number.split("0{1," + Integer.toString(numberOfZeros) + "}$")[0];
         String zeroDecimalPlaces = number.substring(placesBeforeZero.length());
         Label first = new AutoTooltipLabel(placesBeforeZero);
+        if (highlight) first.getStyleClass().add("highlight-text");
         Label last = new Label(zeroDecimalPlaces);
         last.getStyleClass().add("zero-decimals");
-
         return new Tuple2<>(first, last);
     }
 }
