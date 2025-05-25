b5e3c6e10c2dfcfb1e129a1fed26dc12b698086c
woodser <13068859+woodser@users.noreply.github.com>
2025-05-25 08:56:33 -0400

    remove currency codes from crypto names

--- assets/src/main/java/haveno/asset/tokens/TetherUSDERC20.java
+++ assets/src/main/java/haveno/asset/tokens/TetherUSDERC20.java
@@ -6,6 +6,6 @@ public class TetherUSDERC20 extends Erc20Token {
     public TetherUSDERC20() {
         // If you add a new USDT variant or want to change this ticker symbol you should also look here:
         // core/src/main/java/haveno/core/provider/price/PriceProvider.java:getAll()
-        super("Tether USD (ERC20)", "USDT-ERC20");
+        super("Tether USD", "USDT-ERC20");
     }
 }
