e4c14f725cafcdaeec56e1c1abb4038e334035d3
woodser <13068859+woodser@users.noreply.github.com>
2025-06-28 16:37:29 -0400

    randomize trade stats +-10% and within 48 hours (hard fork)

--- core/src/main/java/haveno/core/trade/HavenoUtils.java
+++ core/src/main/java/haveno/core/trade/HavenoUtils.java
@@ -39,6 +39,7 @@ import haveno.core.support.dispute.arbitration.ArbitrationManager;
 import haveno.core.support.dispute.arbitration.arbitrator.Arbitrator;
 import haveno.core.trade.messages.PaymentReceivedMessage;
 import haveno.core.trade.messages.PaymentSentMessage;
+import haveno.core.trade.statistics.TradeStatisticsManager;
 import haveno.core.user.Preferences;
 import haveno.core.util.JsonUtil;
 import haveno.core.xmr.wallet.XmrWalletService;
@@ -132,6 +133,7 @@ public class HavenoUtils {
     public static XmrConnectionService xmrConnectionService;
     public static OpenOfferManager openOfferManager;
     public static CoreNotificationService notificationService;
+    public static TradeStatisticsManager tradeStatisticsManager;
     public static Preferences preferences;
 
     public static boolean isSeedNode() {
