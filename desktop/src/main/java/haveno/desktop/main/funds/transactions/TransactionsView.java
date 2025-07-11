25d75d36b19d2ff61da072844eb7f3194134141d
woodser <13068859+woodser@users.noreply.github.com>
2025-07-11 06:33:36 -0400

    remove 'revert tx' column from transactions view

--- desktop/src/main/java/haveno/desktop/main/funds/transactions/TransactionsView.java
+++ desktop/src/main/java/haveno/desktop/main/funds/transactions/TransactionsView.java
@@ -20,7 +20,6 @@ package haveno.desktop.main.funds.transactions;
 import com.google.inject.Inject;
 import com.googlecode.jcsv.writer.CSVEntryConverter;
 import de.jensd.fx.fontawesome.AwesomeIcon;
-import haveno.common.util.Utilities;
 import haveno.core.api.XmrConnectionService;
 import haveno.core.locale.Res;
 import haveno.core.offer.OpenOffer;
@@ -47,13 +46,11 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.Tooltip;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
@@ -70,7 +67,7 @@ public class TransactionsView extends ActivatableView<VBox, Void> {
     @FXML
     TableView<TransactionsListItem> tableView;
     @FXML
-    TableColumn<TransactionsListItem, TransactionsListItem> dateColumn, detailsColumn, addressColumn, transactionColumn, amountColumn, txFeeColumn, confidenceColumn, memoColumn, revertTxColumn;
+    TableColumn<TransactionsListItem, TransactionsListItem> dateColumn, detailsColumn, addressColumn, transactionColumn, amountColumn, txFeeColumn, confidenceColumn, memoColumn;
     @FXML
     Label numItems;
     @FXML
@@ -137,7 +134,6 @@ public class TransactionsView extends ActivatableView<VBox, Void> {
         txFeeColumn.setGraphic(new AutoTooltipLabel(Res.get("shared.txFee", Res.getBaseCurrencyCode())));
         confidenceColumn.setGraphic(new AutoTooltipLabel(Res.get("shared.confirmations", Res.getBaseCurrencyCode())));
         memoColumn.setGraphic(new AutoTooltipLabel(Res.get("funds.tx.memo")));
-        revertTxColumn.setGraphic(new AutoTooltipLabel(Res.get("shared.revert", Res.getBaseCurrencyCode())));
 
         tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY_FLEX_LAST_COLUMN);
         tableView.setPlaceholder(new AutoTooltipLabel(Res.get("funds.tx.noTxAvailable")));
@@ -151,7 +147,6 @@ public class TransactionsView extends ActivatableView<VBox, Void> {
         setTxFeeColumnCellFactory();
         setConfidenceColumnCellFactory();
         setMemoColumnCellFactory();
-        setRevertTxColumnCellFactory();
 
         dateColumn.setComparator(Comparator.comparing(TransactionsListItem::getDate));
         detailsColumn.setComparator((o1, o2) -> {
@@ -170,10 +165,7 @@ public class TransactionsView extends ActivatableView<VBox, Void> {
         tableView.getSortOrder().add(dateColumn);
 
         keyEventEventHandler = event -> {
-            // Not intended to be public to users as the feature is not well tested
-            if (Utilities.isAltOrCtrlPressed(KeyCode.R, event)) {
-                revertTxColumn.setVisible(!revertTxColumn.isVisible());
-            }
+            // unused
         };
 
         HBox.setHgrow(spacer, Priority.ALWAYS);
@@ -204,7 +196,7 @@ public class TransactionsView extends ActivatableView<VBox, Void> {
         numItems.setText(Res.get("shared.numItemsLabel", sortedDisplayedTransactions.size()));
         exportButton.setOnAction(event -> {
             final ObservableList<TableColumn<TransactionsListItem, ?>> tableColumns = GUIUtil.getContentColumns(tableView);
-            final int reportColumns = tableColumns.size() - 1;    // CSV report excludes the last column (an icon)
+            final int reportColumns = tableColumns.size();
             CSVEntryConverter<TransactionsListItem> headerConverter = item -> {
                 String[] columns = new String[reportColumns];
                 for (int i = 0; i < columns.length; i++)
@@ -495,32 +487,6 @@ public class TransactionsView extends ActivatableView<VBox, Void> {
                                     setGraphic(item.getTxConfidenceIndicator());
                                 } else {
                                     setGraphic(null);
-                                }
-                            }
-                        };
-                    }
-                });
-    }
-
-    private void setRevertTxColumnCellFactory() {
-        revertTxColumn.setCellValueFactory((addressListItem) ->
-                new ReadOnlyObjectWrapper<>(addressListItem.getValue()));
-        revertTxColumn.setCellFactory(
-                new Callback<>() {
-
-                    @Override
-                    public TableCell<TransactionsListItem, TransactionsListItem> call(TableColumn<TransactionsListItem,
-                            TransactionsListItem> column) {
-                        return new TableCell<>() {
-                            Button button;
-
-                            @Override
-                            public void updateItem(final TransactionsListItem item, boolean empty) {
-                                super.updateItem(item, empty);
-                                setGraphic(null);
-                                if (button != null) {
-                                    button.setOnAction(null);
-                                    button = null;
                                 }
                             }
                         };
