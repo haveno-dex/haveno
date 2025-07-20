223988c1e9d9704d792c2a61dec6cd597183b339
woodser <13068859+woodser@users.noreply.github.com>
2025-07-20 10:29:16 -0400

    show error popup when unrestricted monerod has 0 peers

--- desktop/src/main/java/haveno/desktop/main/MainView.java
+++ desktop/src/main/java/haveno/desktop/main/MainView.java
@@ -126,7 +126,6 @@ public class MainView extends InitializableView<StackPane, MainViewModel>  {
     private Label splashP2PNetworkLabel;
     private ProgressBar xmrSyncIndicator;
     private Label xmrSplashInfo;
-    private Popup p2PNetworkWarnMsgPopup, xmrNetworkWarnMsgPopup;
     private final TorNetworkSettingsWindow torNetworkSettingsWindow;
     private final Preferences preferences;
     private static final int networkIconSize = 20;
@@ -713,15 +712,10 @@ public class MainView extends InitializableView<StackPane, MainViewModel>  {
             if (newValue != null) {
                 xmrInfoLabel.setId("splash-error-state-msg");
                 xmrInfoLabel.getStyleClass().add("error-text");
-                if (xmrNetworkWarnMsgPopup == null) {
-                    xmrNetworkWarnMsgPopup = new Popup().warning(newValue);
-                    xmrNetworkWarnMsgPopup.show();
-                }
+                new Popup().warning(newValue).show();
             } else {
                 xmrInfoLabel.setId("footer-pane");
                 xmrInfoLabel.getStyleClass().remove("error-text");
-                if (xmrNetworkWarnMsgPopup != null)
-                    xmrNetworkWarnMsgPopup.hide();
             }
         });
 
@@ -804,10 +798,7 @@ public class MainView extends InitializableView<StackPane, MainViewModel>  {
         p2PNetworkLabel.idProperty().bind(model.getP2pNetworkLabelId());
         model.getP2pNetworkWarnMsg().addListener((ov, oldValue, newValue) -> {
             if (newValue != null) {
-                p2PNetworkWarnMsgPopup = new Popup().warning(newValue);
-                p2PNetworkWarnMsgPopup.show();
-            } else if (p2PNetworkWarnMsgPopup != null) {
-                p2PNetworkWarnMsgPopup.hide();
+                new Popup().warning(newValue).show();
             }
         });
         p2PNetworkIcon.setOnMouseClicked(e -> {
