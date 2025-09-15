298f48e6f6fcfdb0476c524a4b3f48c5333a42c1
woodser <13068859+woodser@users.noreply.github.com>
2025-09-15 10:12:12 -0400

    always sync wallets with progress

--- core/src/main/java/haveno/core/trade/Trade.java
+++ core/src/main/java/haveno/core/trade/Trade.java
@@ -3184,13 +3184,7 @@ public abstract class Trade extends XmrWalletBase implements Tradable, Model {
     private void syncWalletIfBehind() {
         synchronized (walletLock) {
             if (isWalletBehind()) {
-
-                // TODO: local tests have timing failures unless sync called directly
-                if (xmrConnectionService.getTargetHeight() - walletHeight.get() < XmrWalletBase.DIRECT_SYNC_WITHIN_BLOCKS) {
-                    xmrWalletService.syncWallet(wallet); // TODO: always running this causes "Wallet is not connected to daemon" error
-                } else {
-                    syncWithProgress();
-                }
+                syncWithProgress();
                 walletHeight.set(wallet.getHeight());
             }
         }
