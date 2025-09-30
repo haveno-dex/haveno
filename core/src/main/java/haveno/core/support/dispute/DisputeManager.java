eb33b5d9ab08587d1b4548154a4eb27141bef05c
woodser <13068859+woodser@users.noreply.github.com>
2025-09-30 06:30:46 -0400

    remove chat warning when missing dispute trade

--- core/src/main/java/haveno/core/support/dispute/DisputeManager.java
+++ core/src/main/java/haveno/core/support/dispute/DisputeManager.java
@@ -1184,7 +1184,7 @@ public abstract class DisputeManager<T extends DisputeList<Dispute>> extends Sup
         if (dispute.isClosed()) return false;
         Optional<Trade> tradeOptional = findTrade(dispute);
         if (!tradeOptional.isPresent()) {
-            log.warn("Dispute trade {} does not exist", dispute.getTradeId());
+            //log.warn("Dispute trade {} does not exist", dispute.getTradeId());
             return false;
         }
         Trade trade = tradeOptional.get();
