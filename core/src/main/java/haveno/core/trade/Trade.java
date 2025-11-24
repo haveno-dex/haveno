ae7d2189f5d3689829beb5dfb9bff7ff8b07ba7b
woodser <13068859+woodser@users.noreply.github.com>
2025-11-23 19:53:38 -0500

    skip requesting connection switch on error rescanning outputs

--- core/src/main/java/haveno/core/trade/Trade.java
+++ core/src/main/java/haveno/core/trade/Trade.java
@@ -3284,7 +3284,8 @@ public abstract class Trade extends XmrWalletBase implements Tradable, Model {
                     try {
                         rescanSpent(true);
                     } catch (Exception e) {
-                        ThreadUtils.submitToPool(() -> requestSwitchToNextBestConnection(sourceConnection)); // do not block polling thread
+                        // TODO: rescan error is common, e.g. "no connection to daemon"
+                        //ThreadUtils.submitToPool(() -> requestSwitchToNextBestConnection(sourceConnection)); // do not block polling thread
                     }
                 }
 
