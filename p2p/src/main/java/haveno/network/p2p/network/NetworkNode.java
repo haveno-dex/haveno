efce6818004ec31fd00bd40efde0e033df2f79c5
woodser <13068859+woodser@users.noreply.github.com>
2026-03-28 11:56:47 -0400

    p2p: use high-performance sets for server and network node

--- p2p/src/main/java/haveno/network/p2p/network/NetworkNode.java
+++ p2p/src/main/java/haveno/network/p2p/network/NetworkNode.java
@@ -44,13 +44,13 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 import java.io.IOException;
-
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -75,10 +75,10 @@ public abstract class NetworkNode implements MessageListener {
     @Nullable
     private final BanFilter banFilter;
 
-    private final CopyOnWriteArraySet<InboundConnection> inBoundConnections = new CopyOnWriteArraySet<>();
-    private final CopyOnWriteArraySet<MessageListener> messageListeners = new CopyOnWriteArraySet<>();
-    private final CopyOnWriteArraySet<ConnectionListener> connectionListeners = new CopyOnWriteArraySet<>();
-    final CopyOnWriteArraySet<SetupListener> setupListeners = new CopyOnWriteArraySet<>();
+    private final Set<InboundConnection> inBoundConnections = Collections.newSetFromMap(new ConcurrentHashMap<InboundConnection, Boolean>());
+    private final Set<MessageListener> messageListeners = Collections.newSetFromMap(new ConcurrentHashMap<MessageListener, Boolean>());
+    private final Set<ConnectionListener> connectionListeners = Collections.newSetFromMap(new ConcurrentHashMap<ConnectionListener, Boolean>());
+    final Set<SetupListener> setupListeners = Collections.newSetFromMap(new ConcurrentHashMap<SetupListener, Boolean>());
     private final ListeningExecutorService connectionExecutor;
     private final ListeningExecutorService sendMessageExecutor;
     private Server server;
@@ -86,7 +86,7 @@ public abstract class NetworkNode implements MessageListener {
     @Getter
     private volatile boolean isShutDownStarted;
     // accessed from different threads
-    private final CopyOnWriteArraySet<OutboundConnection> outBoundConnections = new CopyOnWriteArraySet<>();
+    private final Set<OutboundConnection> outBoundConnections = Collections.newSetFromMap(new ConcurrentHashMap<OutboundConnection, Boolean>());
     protected final ObjectProperty<NodeAddress> nodeAddressProperty = new SimpleObjectProperty<>();
 
     ///////////////////////////////////////////////////////////////////////////////////////////
