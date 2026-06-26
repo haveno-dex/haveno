c0fe8692375e7c85c7f45dedf4db5f3bb905029f
woodser <13068859+woodser@users.noreply.github.com>
2026-06-25 23:08:33 -0400

    api: support bizum payment method over grpc api

--- core/src/main/java/haveno/core/payment/payload/PaymentMethod.java
+++ core/src/main/java/haveno/core/payment/payload/PaymentMethod.java
@@ -350,6 +350,7 @@ public final class PaymentMethod implements PersistablePayload, Comparable<Payme
     // TODO: delete this override method, which overrides the paymentMethods variable, when all payment methods supported using structured form api, and make paymentMethods private
     public static List<PaymentMethod> getPaymentMethods() {
         List<String> paymentMethodIds = List.of(
+                BIZUM_ID,
                 BLOCK_CHAINS_ID,
                 CASH_AT_ATM_ID,
                 CASH_DEPOSIT_ID,
