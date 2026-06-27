a777f00ac74f95736044ebca5483fd4a1814e0ef
woodser <13068859+woodser@users.noreply.github.com>
2026-06-26 21:22:26 -0400

    api: support tikkie over grpc api

--- core/src/main/java/haveno/core/payment/TikkieAccount.java
+++ core/src/main/java/haveno/core/payment/TikkieAccount.java
@@ -17,21 +17,34 @@
 
 package haveno.core.payment;
 
+import haveno.core.api.model.PaymentAccountForm;
 import haveno.core.api.model.PaymentAccountFormField;
+import haveno.core.locale.Country;
+import haveno.core.locale.CountryUtil;
 import haveno.core.locale.TraditionalCurrency;
 import haveno.core.locale.TradeCurrency;
 import haveno.core.payment.payload.PaymentAccountPayload;
 import haveno.core.payment.payload.PaymentMethod;
 import haveno.core.payment.payload.TikkieAccountPayload;
+import haveno.core.payment.validation.IBANValidator;
 import lombok.EqualsAndHashCode;
 import org.jetbrains.annotations.NotNull;
 
+import javax.annotation.Nullable;
+import java.util.Arrays;
 import java.util.List;
 
 @EqualsAndHashCode(callSuper = true)
 public final class TikkieAccount extends CountryBasedPaymentAccount {
 
     public static final List<TradeCurrency> SUPPORTED_CURRENCIES = List.of(new TraditionalCurrency("EUR"));
+
+    private static final List<PaymentAccountFormField.FieldId> INPUT_FIELD_IDS = List.of(
+            PaymentAccountFormField.FieldId.COUNTRY,
+            PaymentAccountFormField.FieldId.IBAN,
+            PaymentAccountFormField.FieldId.ACCOUNT_NAME,
+            PaymentAccountFormField.FieldId.SALT
+    );
 
     public TikkieAccount() {
         super(PaymentMethod.TIKKIE);
@@ -74,6 +87,24 @@ public final class TikkieAccount extends CountryBasedPaymentAccount {
 
     @Override
     public @NotNull List<PaymentAccountFormField.FieldId> getInputFieldIds() {
-        throw new RuntimeException("Not implemented");
+        return INPUT_FIELD_IDS;
+    }
+
+    @Override
+    @Nullable
+    public List<Country> getSupportedCountries() {
+        return Arrays.asList(CountryUtil.findCountryByCode("NL").get());
+    }
+
+    @Override
+    public void validateFormField(PaymentAccountForm form, PaymentAccountFormField.FieldId fieldId, String value) {
+        switch (fieldId) {
+        case IBAN:
+            // Tikkie is a Dutch service, so the IBAN must be Dutch (matches the desktop TikkieForm's IBANValidator("NL"))
+            processValidationResult(new IBANValidator("NL").validate(value));
+            break;
+        default:
+            super.validateFormField(form, fieldId, value);
+        }
     }
 }
