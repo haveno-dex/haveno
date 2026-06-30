0111d65319737aa032c5bde18ab3000345dcb498
woodser <13068859+woodser@users.noreply.github.com>
2026-06-29 23:03:14 -0400

    api: support moneybeam over grpc api

--- core/src/main/java/haveno/core/api/model/PaymentAccountForm.java
+++ core/src/main/java/haveno/core/api/model/PaymentAccountForm.java
@@ -111,7 +111,8 @@ public final class PaymentAccountForm implements PersistablePayload {
         ADVANCED_CASH,
         CELPAY,
         HAL_CASH,
-        MONESE;
+        MONESE,
+        MONEY_BEAM;
 
         public static PaymentAccountForm.FormId fromProto(protobuf.PaymentAccountForm.FormId formId) {
             return ProtoUtil.enumFromProto(PaymentAccountForm.FormId.class, formId.name());
