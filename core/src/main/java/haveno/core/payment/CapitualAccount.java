c4cd0c417dbcf450b1185cf6e7d92411e5364319
woodser <13068859+woodser@users.noreply.github.com>
2026-07-06 08:33:03 -0400

    api: support capitual over grpc api

--- core/src/main/java/haveno/core/payment/CapitualAccount.java
+++ core/src/main/java/haveno/core/payment/CapitualAccount.java
@@ -17,12 +17,16 @@
 
 package haveno.core.payment;
 
+import haveno.core.api.model.PaymentAccountForm;
 import haveno.core.api.model.PaymentAccountFormField;
+import haveno.core.locale.Res;
 import haveno.core.locale.TraditionalCurrency;
 import haveno.core.locale.TradeCurrency;
 import haveno.core.payment.payload.CapitualAccountPayload;
 import haveno.core.payment.payload.PaymentAccountPayload;
 import haveno.core.payment.payload.PaymentMethod;
+import haveno.core.payment.validation.CapitualValidator;
+import haveno.core.util.validation.RegexValidator;
 import lombok.EqualsAndHashCode;
 import org.jetbrains.annotations.NotNull;
 
@@ -36,6 +40,13 @@ public final class CapitualAccount extends PaymentAccount {
             new TraditionalCurrency("EUR"),
             new TraditionalCurrency("GBP"),
             new TraditionalCurrency("USD")
+    );
+
+    private static final List<PaymentAccountFormField.FieldId> INPUT_FIELD_IDS = List.of(
+            PaymentAccountFormField.FieldId.ACCOUNT_NR,
+            PaymentAccountFormField.FieldId.TRADE_CURRENCIES,
+            PaymentAccountFormField.FieldId.ACCOUNT_NAME,
+            PaymentAccountFormField.FieldId.SALT
     );
 
     public CapitualAccount() {
@@ -56,7 +67,26 @@ public final class CapitualAccount extends PaymentAccount {
     @NotNull
     @Override
     public List<PaymentAccountFormField.FieldId> getInputFieldIds() {
-        throw new RuntimeException("Not implemented");
+        return INPUT_FIELD_IDS;
+    }
+
+    @Override
+    public void validateFormField(PaymentAccountForm form, PaymentAccountFormField.FieldId fieldId, String value) {
+        switch (fieldId) {
+        case ACCOUNT_NR:
+            // the CAP code has a fixed format (mirrors the desktop CapitualValidator)
+            processValidationResult(new CapitualValidator(new RegexValidator()).validate(value));
+            break;
+        default:
+            super.validateFormField(form, fieldId, value);
+        }
+    }
+
+    @Override
+    protected PaymentAccountFormField getEmptyFormField(PaymentAccountFormField.FieldId fieldId) {
+        PaymentAccountFormField field = super.getEmptyFormField(fieldId);
+        if (field.getId() == PaymentAccountFormField.FieldId.ACCOUNT_NR) field.setLabel(Res.get("payment.capitual.cap"));
+        return field;
     }
 
     public void setAccountNr(String accountNr) {
