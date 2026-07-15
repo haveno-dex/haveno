30f10d39e3be81e15800e2e8f0d9d0f480b943ba
woodser <13068859+woodser@users.noreply.github.com>
2026-07-15 08:30:33 -0400

    desktop: encode byte arrays as hex for trade detail data

--- core/src/main/java/haveno/core/util/JsonUtil.java
+++ core/src/main/java/haveno/core/util/JsonUtil.java
@@ -20,13 +20,28 @@ package haveno.core.util;
 import com.google.gson.ExclusionStrategy;
 import com.google.gson.FieldAttributes;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
 import haveno.common.util.JsonExclude;
+import haveno.common.util.Utilities;
 import haveno.core.offer.OfferPayload;
 import haveno.core.trade.Contract;
+import java.lang.reflect.Type;
 
 
 public class JsonUtil {
     public static String objectToJson(Object object) {
+        return objectToJson(object, false);
+    }
+
+    // Display-only variant that encodes byte[] fields as hex strings instead of number arrays.
+    // Must not be used for the signed contract json, which would change the hash and break trades.
+    public static String objectToJsonWithHexBytes(Object object) {
+        return objectToJson(object, true);
+    }
+
+    private static String objectToJson(Object object, boolean hexBytes) {
         GsonBuilder gsonBuilder = new GsonBuilder()
                 .setExclusionStrategies(new AnnotationExclusionStrategy())
                 .setPrettyPrinting();
@@ -34,7 +49,17 @@ public class JsonUtil {
             gsonBuilder.registerTypeAdapter(OfferPayload.class,
                     new OfferPayload.JsonSerializer());
         }
+        if (hexBytes) {
+            gsonBuilder.registerTypeAdapter(byte[].class, new ByteArrayHexSerializer());
+        }
         return gsonBuilder.create().toJson(object);
+    }
+
+    private static class ByteArrayHexSerializer implements com.google.gson.JsonSerializer<byte[]> {
+        @Override
+        public JsonElement serialize(byte[] bytes, Type type, JsonSerializationContext context) {
+            return new JsonPrimitive(Utilities.bytesAsHexString(bytes));
+        }
     }
 
     private static class AnnotationExclusionStrategy implements ExclusionStrategy {
