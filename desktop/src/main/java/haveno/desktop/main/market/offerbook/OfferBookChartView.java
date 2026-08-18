1b25407e3f85b4c84b823ef0c26303f6ee4eed9a
woodser <13068859+woodser@users.noreply.github.com>
2026-08-18 15:29:07 -0400

    desktop: keep table scrollbar thumb clear of rounded bottom corner

--- desktop/src/main/java/haveno/desktop/main/market/offerbook/OfferBookChartView.java
+++ desktop/src/main/java/haveno/desktop/main/market/offerbook/OfferBookChartView.java
@@ -713,7 +713,7 @@ public class OfferBookChartView extends ActivatableViewAndModel<VBox, OfferBookC
 
     private Tuple4<TableView<OfferListItem>, VBox, Button, Label> getOfferTable(OfferDirection direction) {
         TableView<OfferListItem> tableView = new TableView<>();
-        GUIUtil.applyTableStyle(tableView, false);
+        GUIUtil.applyTableStyle(tableView);
         tableView.setMinHeight(initialOfferTableViewHeight);
         tableView.setPrefHeight(initialOfferTableViewHeight);
         tableView.setMinWidth(480);
